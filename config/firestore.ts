f5b1017afd15f34d3439a377843861e7836423e0
Guilherme Gonçalves <guilhermegso1019@gmail.com>
2020-12-03 18:19:10 -0300

    :tada: Insert config gitignore

--- config/firestore.ts
+++
@@ -1,7 +0,0 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import app from './app';
-
-const firestore = firebase.firestore(app);
-
-export default firestore;
--- config/storage.ts
+++
@@ -1,7 +0,0 @@
-import firebase from 'firebase/app';
-import 'firebase/storage';
-import app from './app';
-
-const storage = firebase.storage(app);
-
-export default storage;
